d8bbbf9320a022d83665a3f0aad8d72bdb6f63ca
parkjangwon <vim@kakao.com>
2025-10-07 15:22:47 +0900

    chore: 버전 갱신

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.1.1"
+        versionName = "1.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
