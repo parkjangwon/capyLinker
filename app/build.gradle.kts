3b03283d11be61a6193bb0f2a40eb1f6bfcaa612
parkjangwon <vim@kakao.com>
2025-10-09 13:58:39 +0900

    chore: 빌드 설정 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,13 +19,13 @@ if (localPropertiesFile.exists()) {
 
 android {
     namespace = "org.parkjw.capylinker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "org.parkjw.capylinker"
         minSdk = 26
-        targetSdk = 34
-        versionCode = 1
+        targetSdk = 35
+        versionCode = 2
         versionName = "1.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
