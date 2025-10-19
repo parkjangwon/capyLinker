4cad4b14a9edf01a314f7c733a25dc572fa51378
parkjangwon <vim@kakao.com>
2025-10-19 13:19:54 +0900

    build: 버전 경신

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "org.parkjw.capylinker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
